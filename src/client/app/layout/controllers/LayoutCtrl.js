16dbc5924dc49c6a6686973f3891f8c0b8dc87d6
jainnishi <workspot1992@gmail.com>
2015-08-10 13:50:09 +0530

    Added registration form

--- src/client/app/layout/controllers/LayoutCtrl.js
+++ src/client/app/layout/controllers/LayoutCtrl.js
@@ -6,12 +6,15 @@ define(['layout/module'],function(module){
     $scope.Product={};
     $scope.Product.Category = [];
     $scope.slides = [];
+
     ProductsFactory.getOffers().then(function(response){     
       $scope.slides = response.Offers;                                
     });
+
     ProductsFactory.getProducts().then(function(response){
       $scope.categories = response.Category;
     });
+
     ProductsFactory.getSplProducts().then(function(response){
       $scope.Product.Category.push({'name':"Featured",'products': response.Featured});
       $scope.Product.Category.push({'name':"Latest",'products': response.Latest});
@@ -32,7 +35,6 @@ define(['layout/module'],function(module){
         controller: 'ModalController'
       });
     }; 
-
 
   });
 });
